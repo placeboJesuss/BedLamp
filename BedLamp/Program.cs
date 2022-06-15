06e242b421f03a5c39e049c03525187e7d1799db
Michael <abrakadabra4334@gmail.com>
2022-06-15 13:39:23 +0300

    two test

--- BedLamp/Program.cs
+++ BedLamp/Program.cs
@@ -10,63 +10,31 @@ namespace BedLamp
     class Led
     {
         public int r, g, b;
-         int time;
-        float br;
-
+        
         public Led()
         {
-            r = 100;
-            g = 100;
-            b = 100;
-
-            br = 1;
-            time = 0;
+            r = 0;
+            g = 0;
+            b = 0;
         }
-        public float Bright
-        {
-            get
-            {
-                return br;
-            }
-            set
-            {
-                br = value;
-            }
-        }
-        public void setRGB(int R, int G, int B)
+        public Led(int R, int G, int B)
         {
             r = R;
             g = G;
             b = B;
         }
-        public void setRGB(int R, int G, int B, float bright, int Time)
+      
+        public Led setRGB(int R, int G, int B)
         {
             r = R;
             g = G;
             b = B;
-            br = bright;
-            time = Time;
+            return this;
         }
 
         override public string ToString()
         {
-            return string.Format("{0:X2}{1:X2}{2:X2}", (int)(br * r), (int)(br * g), (int)(br * b));
-        }
-        public void Tick(int deltaTime)
-        {
-            Random rand = new Random();
-            br -= 1/30f;
-            int ll = 2;
-            r = (r + rand.Next(0, ll))%255;
-            g  =(g + rand.Next(0, ll)) % 255;
-            b = (b + rand.Next(0, ll)) % 255;
-            if (br <= 0.05)
-            {
-                br = 0;
-             //  r = (byte)(rand.Next(0, 255));
-              //  g = (byte)(rand.Next(0, 255));
-             //   b = (byte)(rand.Next(0, 255));
-            }
+            return string.Format("{0:X2}{1:X2}{2:X2}", (int)(r), (int)(g), (int)(b));
         }
     }
 
@@ -74,18 +42,12 @@ namespace BedLamp
     class Program
     {
         static int n = 60;
-        static byte[] data;
-        static Led[] leds;
-        static UdpClient client;
         static void Main(string[] args)
         {
-            client = new UdpClient("192.168.0.169", 4210);
+            UdpClient  client = new UdpClient("192.168.0.169", 4210);
             Console.WriteLine("Hello World!");
             MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
             MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-            leds = new Led[n];
-            for (int i = 0; i < n; i++)
-                leds[i] = new Led();
             float[] chek = new float[500];
             int pos = 0;
             float sum = 0, max = 0;
@@ -94,7 +56,9 @@ namespace BedLamp
                 float level = defaultDevice.AudioMeterInformation.MasterPeakValue;
                 if(level>0.001)
                     add(chek, level, ref pos, ref sum, ref max);
-                musicGradient(level, sum/chek.Length , max);
+                byte[] data = musicGradient(level, sum/chek.Length , max);
+                if(data!= null)
+                    client.Send(data, data.Length);
                 System.Threading.Thread.Sleep(10);
             }
 
@@ -118,18 +82,18 @@ namespace BedLamp
         }
 
 
-        static int cc = 0;
       
         static int br;
         static float counter;
         static int s;
         static DateTime dt;
-       
 
-
-
-        private static void musicGradient(float brr, float sum, float max)
+        /// <summary>
+        /// Form data for UDP request
+        /// </summary>
+        private static byte[] musicGradient(float brr, float sum, float max)
         {
+            Led led = new Led();
             int bright = (int)(brr * 255);
             string request = "2";
             int rr = bright;
@@ -141,7 +105,7 @@ namespace BedLamp
             if (rr > br) br = (int)(br * 0.3 + rr * 0.7);
             else br = (int)(br * 0.8 + rr * 0.2);
             int i;
-            if (s == 0 && (int)(n / 2 * br / 255f + 0.5) == 0) return;
+            if (s == 0 && (int)(n / 2 * br / 255f + 0.5) == 0) return null;
             s = (int)(n / 2 * Math.Pow(br / 255f,1.4f)  + 0.5);
 
             Console.WriteLine(rr.ToString() + " " + s.ToString());
@@ -150,158 +114,26 @@ namespace BedLamp
 
             for (i = 0; i < s; i++)
             {
-                HsvToRgb(counter - i * 8, 1, ((0.2 + 0.8 * (s - i) / s) * (s*2f/n)), out r, out g, out b);
-                leds[i].setRGB(r,g,b);
-                request += leds[i].ToString();
+                double hue = counter - i * 8;
+                double value = ((0.2 + 0.8 * (s - i) / s) * (s * 2f / n));
+                HsvToRgb(hue,1,value,out r, out g, out b);
+                request += led.setRGB(r,g,b).ToString();
             }
             for (; i < n / 2; i++)
             {
-                leds[i].setRGB(0, 0, 0);
-                request += leds[i].ToString();
+                request += led.setRGB(0, 0, 0).ToString();
             }
-            data = Encoding.UTF8.GetBytes(request);
-            cc++;
-            client.Send(data, data.Length);
-
+            
             counter +=  (float)(DateTime.Now - dt).TotalMilliseconds / 7f * s/30f;
             dt = DateTime.Now;
             if (counter >= 360) counter = 0;
-
-        }
-        static void round()
-        {
-            for (int i = 0; i < n / 2-1; i++)
-            {
-                leds[i].Bright = leds[i + 1].Bright;
-                
-                leds[n-1 - i].Bright = leds[n - i - 2].Bright;
-
-            }
-            string request = "1";
-            for (int i = 0; i < n; i++)
-                request += leds[i].ToString();
-            data = Encoding.UTF8.GetBytes(request);
-            client.Send(data, data.Length);
-        }
-        static void lines(int bright)
-        {
-            if (bright / 255f > 0.05)
-            {
-                leds[n / 2].Bright = (float)Math.Pow(bright / 255f, 3);
-                leds[n / 2 - 1].Bright = (float)Math.Pow(bright / 255f, 3);
-            }
-            else
-            {
-                leds[n / 2].Bright = 0f;
-                leds[n / 2 - 1].Bright = 0f;
-            }
+            
+            return Encoding.UTF8.GetBytes(request);
         }
 
-        //private static void plugin()
-        //{
-        //    while (port.BytesToRead > 0)
-        //    {
-        //        while (port.ReadByte() == 65)
-        //            if (port.ReadByte() == 100)
-        //                if (port.ReadByte() == 97)
-        //                    if (port.ReadByte() == 0)
-        //                    {
-        //                        string request = "2";
-        //                        int num;
-        //                        num = port.ReadByte() + 1;
-        //                        port.ReadByte();
-        //                        int br = 0;
-        //                        for (int j = 0; j < num; j++)
-        //                        {
-        //                            br += port.ReadByte();
-
-        //                            port.ReadByte();
-        //                            port.ReadByte();
-        //                        }
-        //                        br = br / num;
-        //                        int s = (int)(n / 2 * br / 255f + 0.5);
-        //                        int i,r,g,b;
-        //                        for (i = 0; i < s; i++)
-        //                        {
-        //                            HsvToRgb(counter - i * 10, 1, ((0.2 + 0.8 * (s - i) / s) *br / 255f), out r, out g, out b);
-        //                            request += string.Format("{0:X2}{1:X2}{2:X2}", (int)(r), (int)(g), (int)(b));
-
-        //                        }
-        //                        // request += string.Format("{0:X2}{1:X2}{2:X2}", (int)(255), (int)(0), (int)(0));
-        //                        for (; i < n / 2; i++)
-        //                        {
-        //                            request += string.Format("{0:X2}{1:X2}{2:X2}", (int)(0), (int)(0), (int)(0));
-
-        //                        }
-
-        //                        data = Encoding.UTF8.GetBytes(request);
-        //                        cc++;
-        //                        client.Send(data, data.Length);
-        //                        counter += 3.5f * br / 255f;
-        //                       // counter += 7f * br / 255f;
-        //                        if (counter >= 360) counter = 0;
-        //                    }
-        //    }
-        //}
-        //private static void direct()
-        //{
-        //    while (port.BytesToRead > 0)
-        //    {
-        //        while (port.ReadByte() == 65)
-        //            if (port.ReadByte() == 100)
-        //                if (port.ReadByte() == 97)
-        //                    if (port.ReadByte() == 0)
-        //                    {
-        //                        string request = "1";
-        //                        int num;
-        //                        num = port.ReadByte() + 1;
-        //                        port.ReadByte();
-        //                        for (int j = 0; j < 30; j++)
-        //                        {
-        //                            int r = port.ReadByte();
-        //                            int g = port.ReadByte();
-        //                            int b = port.ReadByte();
-
-        //                            request += string.Format("{0:X2}{1:X2}{2:X2}", (int)(r), (int)(g), (int)(b));
-        //                        }
-        //                        data = Encoding.UTF8.GetBytes(request);
-        //                        client.Send(data, data.Length);
-        //                        n++;
-        //                    }
-        //    }
-        //}
-
-
-        //    private static void Sp_DataReceeived(object sender, SerialDataReceivedEventArgs e)
-        //    {
-        //        int bytes = sp.BytesToRead;
-        //        for (int i = 0; i < bytes; i++)
-        //        {
-        //            while (sp.ReadByte() == 65)
-        //                if (sp.ReadByte() == 100)
-        //                    if (sp.ReadByte() == 97)
-        //                        if (sp.ReadByte() == 0)
-        //                        {
-        //                            string request;
-        //                            int num;
-        //                            num = sp.ReadByte() + 1;
-        //                            request = num.ToString();
-        //                            sp.ReadByte();
-        //                            for (int j = 0; j < num * 3; j++)
-        //                            {
-        //                                int r = sp.ReadByte();
-        //                                int g = sp.ReadByte();
-        //                                int b = sp.ReadByte();
-
-        //                                request += "&" + string.Format("{0:X2}{1:X2}{2:X2}", (int)(r), (int)(g), (int)(b));
-        //                            }
-        //                            data = Encoding.UTF8.GetBytes(request);
-        //                            client.Send(data, data.Length);
-        //                            n++;
-        //                        }
-        //        }
-        //    }
-
+        /// <summary>
+        /// Convert HSV to RGB
+        /// </summary>
         static void HsvToRgb(double h, double S, double V, out int r, out int g, out int b)
         {
             double H = h;
@@ -402,75 +234,6 @@ namespace BedLamp
             if (i > 255) return 255;
             return i;
         }
-
-        //    private static void Sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        //    {
-        //        int bytes = sp.BytesToRead;
-        //        for (int i = 0; i < bytes; i++)
-        //        {
-        //            while (sp.ReadByte() == 65)
-        //                if (sp.ReadByte() == 100)
-        //                    if (sp.ReadByte() == 97)
-        //                        if (sp.ReadByte() == 0)
-        //                        {
-        //                            string request;
-        //                            int num;
-        //                            num = 30;
-        //                            request = num.ToString();
-        //                            sp.ReadByte();
-        //                            sp.ReadByte();
-        //                            char r = (char)sp.ReadByte();
-
-        //                            sp.ReadByte();
-
-        //                            sp.ReadByte();
-        //                            sp.ReadByte();
-        //                            char g = (char)sp.ReadByte();
-        //                            sp.ReadByte();
-        //                            sp.ReadByte();
-        //                            sp.ReadByte();
-        //                            char b = (char)sp.ReadByte();
-
-        //                            //  br = (r + g + b) / 765f * 0.65f + br * 0.35f ;
-        //                            if ((r + g + b) / 765f >= br)
-        //                            {
-        //                                br = (r + g + b) / 765f;
-        //                            }
-        //                            else
-        //                            {
-        //                                br = (r + g + b) / 765f * 0.6f + br * 0.4f; ;
-        //                                // if (br < 0) br = 0;
-        //                            }
-        //                            int fn = (int)(num * br);
-        //                            string[] mas = new string[fn / 2];
-        //                            for (int j = 0; j < fn / 2; j++)
-        //                                mas[j] = "&" + string.Format("{0:X2}{1:X2}{2:X2}", (int)(r * br), (int)(g * br), (int)(b * br));
-        //                            for (int j = 0; j < (num / 2 - fn / 2); j++)
-        //                                request += "&" + string.Format("{0:X2}{1:X2}{2:X2}", 0, 0, 0);
-        //                            for (int j = fn / 2 - 1; j >= 0; j--)
-        //                            {
-        //                                request += mas[j];
-        //                            }
-        //                            for (int j = 0; j < fn / 2; j++)
-        //                            {
-        //                                request += mas[j];
-        //                            }
-        //                            for (int j = 0; j < (num / 2 - fn / 2); j++)
-        //                                request += "&" + string.Format("{0:X2}{1:X2}{2:X2}", 0, 0, 0);
-        //                            counter++;
-        //                            if (counter >= 360) counter = 0;
-
-
-        //                            Console.WriteLine(request.Length);
-        //                            //Console.WriteLine(br);
-        //                            //Console.WriteLine("ok");
-        //                            data = Encoding.UTF8.GetBytes(request);
-        //                            client.Send(data, data.Length);
-        //                            n++;
-        //                        }
-        //        }
-        //    }
-        //}
     }    
 
 }
